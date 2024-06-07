26d9e184db9135cecc739ecc60ce37c518ec8245
DaveyJH <daveyj.dh@googlemail.com>
2024-06-07 16:25:51 +0100

    feat: Add comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()

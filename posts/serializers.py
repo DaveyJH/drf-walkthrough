d2cd1c5cd2fd52956004d20794dfba5e395f263b
DaveyJH <daveyj.dh@googlemail.com>
2024-06-10 15:23:47 +0100

    feat: Add following and liked id fields

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):  # _<model field> will run automatically
         if value.size > 2_097_152:
@@ -27,6 +29,16 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context["request"]
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            liked = Like.objects.filter(
+                owner=user,
+                post=obj,
+            ).first()
+            return liked.id if liked else None
+        return None
+
     class Meta:
         model = Post
         fields = [
@@ -41,4 +53,5 @@ class PostSerializer(serializers.ModelSerializer):
             "profile_id",
             "profile_image",
             "image_filter",
+            "like_id",
         ]

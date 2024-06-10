9f1de3ab39a4199abd1bda78d57603563c44a676
DaveyJH <daveyj.dh@googlemail.com>
2024-06-10 18:33:07 +0100

    feat: Convert Post filter to class

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,45 @@
 from django.db.models import Count
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import (
+    ChoiceFilter,
+    FilterSet,
+    DjangoFilterBackend
+)
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+
+
+class PostFilter(FilterSet):
+    profiles = [*Post.objects.all().order_by("owner")]
+    profile_choices = [
+        (prof.owner.id, prof.owner.username,)
+        for prof in profiles
+    ]
+    followed_by = ChoiceFilter(
+        label="Followed by",
+        field_name="owner__followed__owner__profile",
+        choices=profile_choices
+    )
+    liked_by = ChoiceFilter(
+        label="Liked by",
+        field_name="likes__owner__profile",
+        choices=profile_choices
+    )
+    authored_by = ChoiceFilter(
+        label="Authored by",
+        field_name="owner__profile",
+        choices=profile_choices
+    )
+
+    class Meta:
+        fields = [
+            "followed_by",
+            "liked_by",
+            "authored_by",
+        ]
+        model = Post
 
 
 class PostList(generics.ListAPIView):
@@ -19,11 +54,12 @@ class PostList(generics.ListAPIView):
         filters.SearchFilter,
         DjangoFilterBackend
     ]
-    filterset_fields = [
-        "owner__followed__owner__profile",
-        "likes__owner__profile",
-        "owner__profile",
-    ]
+    # filterset_fields = [
+    #     "owner__followed__owner__profile",
+    #     "likes__owner__profile",
+    #     "owner__profile",
+    # ]
+    filterset_class = PostFilter
     ordering_fields = [
         "comments_count",
         "likes_count",

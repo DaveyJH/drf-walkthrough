871515f9f05e5ac373f290d2f6b83704536646db
DaveyJH <daveyj.dh@googlemail.com>
2024-06-13 22:45:14 +0100

    feat: Set DEBUG to False

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -30,7 +30,7 @@ HOST = os.environ.get("HOST")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEVELOPMENT
 
 ALLOWED_HOSTS = [HOST,]
 

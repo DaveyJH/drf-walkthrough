96154ba4a30618e75889acb897de3b5ac7f0bb4e
DaveyJH <daveyj.dh@googlemail.com>
2024-06-17 17:34:18 +0100

    feat: Add react to static sattings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -192,9 +192,9 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 STATIC_URL = '/react/static/'
 if 'DEBUG' in os.environ:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'react/static'),)
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'react/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
